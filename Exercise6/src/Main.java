d6f9beae7d7a62d27edc19e1b5b1d6fc1f95b6d2
Deniz Öğüt <denizogut123@gmail.com>
2022-05-29 07:28:28 +0300

    Olasılık hesaplandı.

--- Exercise6/src/Main.java
+++ Exercise6/src/Main.java
@@ -1,46 +1,61 @@
 public class Main {
     public static void main(String[] args)
     {
-        CrapsSimulator.run();
+        CrapsSimulator.run(100_000,CrapsSimulator.craps());
     }
 }
 
 class CrapsSimulator{
 
-    public static void run ()
+
+    public static void run(int tourCount, int winningCount)
     {
+        double p = winningCount / (double)tourCount;
+        System.out.format("Probability --> %f%n",p);
+    }
+
+    public static int craps ()
+    {
+
         int result = 0;
         int winningCounter = 0;
         int loosingCounter = 0;
+        for(int i = 1; i <= 100_000; i++) {
 
-        while(true) {
-            Dice d1 = new Dice();
-            Dice d2 = new Dice();
-            result = d1.value + d2.value;
-            if (winningCondition(d1,d2))
-            {
-                winningCounter++;
-                System.out.format("Sum --> %d || winningCounter --> %d%n ",result,winningCounter);
-                break;
-            } else if(loosingCondition(d1,d2)) {
-                loosingCounter++;
-                System.out.format("Sum --> %d || loosingCounter --> %d%n ",result,loosingCounter);
-                break;
-            } else {
-                System.out.format("RESULT: %d%n",result);
-                while(true) {
-                    int tempResult = d1.value + d2.value;
-                    if (tempResult == 7 ) {
-                        loosingCounter++;
-                        break;
+            while(true) {
+                Dice d1 = new Dice();
+                Dice d2 = new Dice();
+                result = d1.value + d2.value;
+                if (winningCondition(d1,d2))
+                {
+                    winningCounter++;
+                    //System.out.format("Sum --> %d || winningCounter --> %d%n ",result,winningCounter);
+                    break;
+                } else if(loosingCondition(d1,d2)) {
+                    loosingCounter++;
+                    //System.out.format("Sum --> %d || loosingCounter --> %d%n ",result,loosingCounter);
+                    break;
+                } else {
+                    //System.out.format("RESULT: %d%n",result);
+                    while(true) {
+                        int tempResult = d1.value + d2.value;
+                        if (tempResult == 7 ) {
+
+                            loosingCounter++;
+                            //System.out.format("tempResult --> %d || loosingCounter --> %d%n ",tempResult,loosingCounter);
+                            break;
+                        }
+
+                        if (tempResult == result)
+                            winningCounter++;
+                            //System.out.format("tempResult --> %d || winningCounter --> %d%n ",tempResult,loosingCounter);
+                            break;
                     }
-
-                    if (tempResult == result)
-                        winningCounter++;
-                        break;
                 }
             }
         }
+
+            return winningCounter;
     }
 
     public static boolean winningCondition(Dice d1, Dice d2)
